3c062a7b64fd7467fa230aca4829d4aa31a0ac3f
1997808 <khanhhuyle22@gmail.com>
2022-01-31 16:06:21 +0700

    404 page fine

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import error from "../assets/error.png"
 
 export const Page404 = () => {
   return (
-    <div className="">
-      <p>404</p>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
+    <div className="w-full">
+      <div className="container mx-auto w-full py-40 flex flex-col justify-between items-center">
+        <img className="pb-4" src={error} alt="404" />
+        <div className="w-40 h-12 flex justify-center items-center bg-logo-orange rounded">
+          <Link to="/"><p className="text-white">Trở về</p></Link>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'logo-orange': '#ff7555',
+      },
+    },
   },
   plugins: [],
 }
