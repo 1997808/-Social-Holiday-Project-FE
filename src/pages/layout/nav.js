fe4bfdfe1624deb70304c555a5a6f395b057e94e
1997808 <khanhhuyle22@gmail.com>
2022-03-18 10:51:21 +0700

    fix sidenav

--- src/pages/layout/nav.js
+++ src/pages/layout/nav.js
@@ -5,15 +5,11 @@ import { Sidenav } from "../../components/sidenav";
 export const NavLayout = () => {
   return (
     <>
-      <div className="grid grid-cols-3 lg:grid-cols-4 gap-4">
-        <div className="col-span-1">
-          <Sidenav />
-        </div>
-        <div className="col-span-3">
-          <div className="flex flex-col justify-between min-h-screen bg-gray-100">
-            <Outlet />
-          </div>
-        </div>
+      <div className="min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidenav />
+        {/* <div className="flex flex-col justify-between min-h-screen bg-gray-100"> */}
+        <Outlet />
+        {/* </div> */}
       </div>
     </>
   );
