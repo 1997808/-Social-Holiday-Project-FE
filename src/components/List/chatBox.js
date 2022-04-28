47def68edb56425c66c057e7be60b382f8e88570
1997808 <khanhhuyle22@gmail.com>
2022-04-28 18:10:49 +0700

    check scroll on top of div

--- src/components/List/chatBox.js
+++ src/components/List/chatBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { MyAxios } from "../../utils/api";
 import { ChatCard } from "../Card/chatCard";
 import { ChatForm } from "../Form/chatForm";
@@ -7,6 +7,13 @@ export const ChatBox = ({ conversationId }) => {
   const [conversation, setConversation] = useState({})
   const [count, setCount] = useState(0)
   const [chats, setChats] = useState([])
+  // const [loadMore, setLoadMore] = useState(true);
+  const listChatRef = useRef();
+
+  // useEffect(() => {
+  //   getMessage(loadMore);
+  //   setLoadMore(false);
+  // }, [loadMore]);
 
   useEffect(() => {
     const getFriend = async () => {
@@ -40,14 +47,26 @@ export const ChatBox = ({ conversationId }) => {
     getMessage();
   }, [conversationId]);
 
+  const onScroll = () => {
+    if (listChatRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = listChatRef.current;
+      // console.log(clientHeight - scrollTop, scrollHeight)
+      if (clientHeight - scrollTop + 1 === scrollHeight) {
+        console.log("reached top");
+      }
+    }
+  };
+
   return (
     <div className="w-full bg-white rounded h-screen">
       <div className="flex justify-between items-center p-5 py-6 bg-logo-orange rounded-t">
         <p className="font-bold text-white">{conversation.title}</p>
       </div>
       <div
-        className="w-full overflow-y-scroll flex flex-col-reverse border-b border-solid border-gray-200"
+        className="w-full overflow-y-auto flex flex-col-reverse border-b border-solid border-gray-200"
         style={{ height: "80vh" }}
+        onScroll={onScroll}
+        ref={listChatRef}
       >
         {chats && chats.map(item => {
           if (item.author) {
